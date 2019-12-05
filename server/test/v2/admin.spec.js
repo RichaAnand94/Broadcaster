594f872809a2da2616c7aad8fc64785a2ea58ef9
RichaAnand94 <richameo@gmail.com>
2019-12-05 10:45:42 +0200

    add test for database

---
+++ server/test/v2/admin.spec.js
@@ -0,0 +1,47 @@
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+
+import server from '../../../server';
+
+import users from '../mock-data/users.json';
+
+chai.use(chaiHttp);
+
+let userToken;
+
+describe('API endpoint /v2/admin', () => {
+
+    before((done) => {
+        chai.request(server)
+            .post('/api/v2/auth/signin')
+            .send(users[0])
+            .then((res) => {
+                userToken = res.body.data.token;
+                done();
+            }).catch(done);
+    });
+
+    it('need to get array while retrieving red-flags by admin', (done) => {
+        chai.request(server)
+            .get('/api/v2/admin/red-flags')
+            .set('token', `${userToken}`)
+            .then((res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.an('object');
+                expect(res.body.data).to.be.an('array');
+                done();
+            }).catch(done);
+    });
+
+    it('need to get array while retrieving interventions by admin', (done) => {
+        chai.request(server)
+            .get('/api/v2/admin/interventions')
+            .set('token', `${userToken}`)
+            .then((res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.an('object');
+                expect(res.body.data).to.be.an('array');
+                done();
+            }).catch(done);
+    });
+});
