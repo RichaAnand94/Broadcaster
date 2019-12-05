4dc438a12d84df907e77b40d4852bcc539af0388
RichaAnand94 <richameo@gmail.com>
2019-12-05 10:30:48 +0200

    add functions for database

--- server/src/v2/controllers/user.controller.js
+++ server/src/v2/controllers/user.controller.js
@@ -1,9 +1,36 @@
-import Pool from 'pg';
+import UserModel from '../models/User';
+import { generateToken } from '../middleware/authorize';
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'testdb',
-    password: '1994',
-    port: '5432',
-})
+const signUp = async (req, res) => {
+    if (!req.body.firstname || !req.body.lastname || !req.body.email || !req.body.phone_number || !req.body.password) {
+        return res.status(401).send({ 'status': 401, 'error': 'All fields are required' })
+    } else if (!await UserModel.checkForPropertyExistence('email', req.body.email)) {
+        if (!await UserModel.checkForPropertyExistence('phone_number', req.body.phone_number)) {
+            const userData = await UserModel.create(req.body);
+            delete userData.password;
+            const token = generateToken(userData);
+            console.log('User created');
+            return res.status(200).send({ 'status': 200, 'message': 'User created successfully', data: { 'token': token } })
+        } else {
+            return res.status(401).send({ 'status': 401, 'error': 'Phone number already exists' })
+        }
+    } else {
+        return res.status(401).send({ 'status': 401, 'error': 'Email already exists' })
+    }
+}
+
+const signIn = async (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(401).send({ 'status': 401, 'error': 'All fields are required' })
+    } else {
+        const userData = await UserModel.findOne('email', req.body.email);
+        if (!userData || userData.password !== req.body.password) {
+            return res.status(401).send({ 'status': 401, 'error': 'Invalid credentials' })
+        } else {
+            delete userData.password;
+            const token = generateToken(userData);
+            return res.status(200).send({ 'status': 200, 'message': 'User logged in successfully', data: { 'token': token } })
+        }
+    }
+}
+export {signUp, signIn};
